2b0e3062e08ecc70579ebc0dd74677915dfa57d9
MarkTheIronMan <medvedevkn@gmail.com>
2020-03-05 23:02:51 +0300

    added flipbyte

---
+++ lab1/flipbyte/Tests.bat
@@ -0,0 +1,47 @@
+@echo off
+
+REM Путь к тестируемой программе передаётся через 1 й аргумент командной строки
+set MyProgram="%~1"
+
+REM Защита от запуска без аргумента, задающего путь к программе
+if %MyProgram%=="" (
+	echo "Please specify path to program"
+	exit /B 1
+) 
+
+REM numeric input "6"
+%MyProgram% "6" > nul || goto err
+echo Test 1 passed
+
+REM num+string input
+%MyProgram% "1ab" > nul || goto err
+echo Test 2 passed
+
+REM num+string input
+%MyProgram% "1 abc" > nul || goto err
+echo Test 3 passed
+
+REM string input
+%MyProgram% "another string" > nul && goto err
+echo Test 4 passed
+
+REM num less than min + string input
+%MyProgram% "-1 abc" > nul && goto err
+echo Test 5 passed
+
+REM num more than max + string input
+%MyProgram% "999 abc" > nul && goto err
+echo Test 6 passed
+
+REM empty
+%MyProgram% > nul && goto err
+echo Test 7 passed
+
+REM Тесты прошли успешно
+echo All tests passed successfuly
+exit /B 0
+
+REM Сюда попадаем в случае ошибки
+:err
+echo Test failed
+exit /B 1
---
+++ lab1/flipbyte/flipbyte.cpp
@@ -0,0 +1,69 @@
+﻿// flipbyte.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+#include <iostream>
+#include <optional>
+#include <string>
+
+using namespace std;
+
+optional<int> ParseArg(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		cout << "Incorrect number of arguments" << endl;
+		return nullopt;
+	}
+
+	int number;
+
+	try
+	{
+		number = stoi(argv[1]);
+	}
+	catch (const invalid_argument& e)
+	{
+		cout << "Error: " << e.what() << ". Argument must be a number" << endl;
+		return nullopt;
+	}
+	catch (const exception& e)
+	{
+		cout << "Error: " << e.what() << endl;
+		return nullopt;
+	}
+	if ((number > UINT8_MAX) || (number < 0))
+	{
+		return nullopt;
+	}
+	return number;
+}
+
+uint8_t FlipByte(uint8_t number)
+{
+	uint8_t result = 0;
+
+	for (int i = 0; i < 8; ++i)
+	{
+		result |= (number & 1) << (7 - i);
+		number >>= 1;
+	}
+
+	return result;
+}
+
+
+int main(int argc, char* argv[])
+{
+	auto arg = ParseArg(argc, argv);
+
+	if (!arg)
+	{
+		cout << "Invalid argument! \n Usage: flipbyte.exe <number in range 0..255>\n";
+		return 1;
+	}
+
+	uint8_t byteNumber = static_cast<uint8_t>(*arg);
+	int newNumber = FlipByte(byteNumber);
+	cout << newNumber << endl;
+
+	return 0;
+}
