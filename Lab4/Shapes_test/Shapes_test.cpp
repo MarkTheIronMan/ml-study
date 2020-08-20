343f9976569b907a6d8ef1382b644ebc65ebe9d0
MarkTheIronMan <medvedevkn@gmail.com>
2020-08-20 19:53:07 +0300

    upd

---
+++ Lab4/Shapes_test/Shapes_test.cpp
@@ -0,0 +1,17 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "../../catch2/catch.hpp"
+#include <iostream>
+#include <math.h>
+#include <string>
+
+#include "../CCircle.h"
+#include "../CLineSegment.h"
+#include "../CPoint.h"
+#include "../CRectangle.h"
+#include "../CTriangle.h"
+
+TEST_CASE("Test circle")
+{
+	CCircle circle(CPoint(100.0, 100.0), 10.0, "000000", "000000");
+	REQUIRE(circle.GetArea() == M_PI * 10.0 * 10.0);
+}
